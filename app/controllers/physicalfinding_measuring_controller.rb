1c5e7c46ad1db7a5c0160ed75703e35086e263b2
mayumiasakawa <m.a.mayumi214@gmail.com>
2021-01-02 23:21:12 +0900

    physicalfinding controller

--- app/controllers/physicalfinding_measuring_controller.rb
+++ app/controllers/physicalfinding_measuring_controller.rb
@@ -1,2 +1,18 @@
 class PhysicalfindingMeasuringController < ApplicationController
+
+  def new_physicalfinding
+    @overview = Overview.new
+  end
+
+  def create
+    Overviews.create(overviews_pamars)
+  end
+
+  private
+
+  def overviews_pamars
+   @overview_physicalfinding_measurings = params.require(:overview).permit(:physicalfinding_measuring_date, :weight, :height, :abdominal_circumference,:bmi,).merge(user_id: current_user.id, overview_id: params[:overview_id])
+   @overview_physicalfinding_measurings = Overview.includes(:user).where.not(physicalfinding_measuring_date:nil).order(physicalfinding_measuring_date: :desc).where(params[:id])
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'overviews#index'
   resources :overviews do
-    resources :physicalfinding_measurings, only: :create
+    resources :physicalfinding_measurings
     resources :blood_urine_tests, only: :create
     resources :medical_cares, only: :create
     resources :vaccines, only: :create
