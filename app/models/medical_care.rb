c6a95a820c6e89da1128cee139dca7f9e346a608
mayumiasakawa <m.a.mayumi214@gmail.com>
2021-01-03 21:58:51 +0900

    medicalcare validate

--- app/models/medical_care.rb
+++ app/models/medical_care.rb
@@ -1,4 +1,16 @@
 class MedicalCare < ApplicationRecord
-  belong_to :user
-  belong_to :overview
+  belongs_to :user
+  belongs_to :overview
+
+  with_options presence: true do         
+    validates :medical_care_date,
+               if: -> { (clinic_name.present? && disease_name.present? && treatment_medicine.present?) ||
+                        (physicalfinding_measuring_date.blank? && blood_urine_test_date.blank? && vaccine_date.blank?) }
+    validates :clinic_name,length: { maximum: 40 },
+              if: -> { medical_care_date.present? || disease_name.present? || treatment_medicine.present? }
+    validates :disease_name,length: { maximum: 60 },
+              if: -> { medical_care_date.present? || clinic_name.present? || treatment_medicine.present? }
+    validates :treatment_medicine,length: { maximum: 60 },
+               if: -> { medical_care_date.present? || clinic_name.present? || disease_name.present? }
+  end
 end
--- app/models/overview.rb
+++ app/models/overview.rb
@@ -13,17 +13,6 @@ class Overview < ApplicationRecord
 
   
   with_options presence: true do
-
-              
-    validates :medical_care_date,
-               if: -> { (clinic_name.present? && disease_name.present? && treatment_medicine.present?) ||
-                        (physicalfinding_measuring_date.blank? && blood_urine_test_date.blank? && vaccine_date.blank?) }
-    validates :clinic_name,length: { maximum: 40 },
-              if: -> { medical_care_date.present? || disease_name.present? || treatment_medicine.present? }
-    validates :disease_name,length: { maximum: 60 },
-              if: -> { medical_care_date.present? || clinic_name.present? || treatment_medicine.present? }
-    validates :treatment_medicine,length: { maximum: 60 },
-               if: -> { medical_care_date.present? || clinic_name.present? || disease_name.present? }
   
     validates :vaccine_date,if: -> { physicalfinding_measuring_date.blank? && blood_urine_test_date.blank? && medical_care_date.blank? },
                             if: :vaccine_type?
