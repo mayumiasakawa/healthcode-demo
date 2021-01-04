0daad4cfcc8a730d081ab1508ed645c543008e04
mayumiasakawa <m.a.mayumi214@gmail.com>
2021-01-04 21:32:05 +0900

    params edit

--- app/controllers/physicalfinding_measurings_controller.rb
+++ app/controllers/physicalfinding_measurings_controller.rb
@@ -1,11 +1,11 @@
 class PhysicalfindingMeasuringsController < ApplicationController
 
   def new
-    @physicalfindingmeasuring = PhysicalfindingMeasurings.new
+    @physicalfindingmeasuring = PhysicalfindingMeasuring.new
   end
 
   def create
-    @physicalfindingmeasuring = PhysicalfindingMeasurings.create(overview_params)
+    @physicalfindingmeasuring = PhysicalfindingMeasuring.create(overview_params)
     if @physicalfindingmeasuring.valid?
       @physicalfindingmeasuring.save
       redirect_to root_path
@@ -17,6 +17,6 @@ class PhysicalfindingMeasuringsController < ApplicationController
   private
 
   def overviews_pamars
-   params.require(:physicalfindingmeasurings).permit(:physicalfinding_measuring_date, :weight, :height, :abdominal_circumference,:bmi).merge(user_id: current_user.id, oveivew_id: params[:overview_id])
+   params.require(:physicalfindingmeasuring).permit(:physicalfinding_measuring_date, :height, :weight, :abdominal_circumference,:bmi).merge(user_id: current_user.id, oveivew_id: params[:overview_id])
   end
 end
