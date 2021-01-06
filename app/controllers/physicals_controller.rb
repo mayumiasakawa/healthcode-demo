9b3c4ebd95b35976e85b6c81e002ec40bf808238
mayumiasakawa <m.a.mayumi214@gmail.com>
2021-01-06 15:39:41 +0900

    physical_controller create

--- app/controllers/physicals_controller.rb
+++ app/controllers/physicals_controller.rb
@@ -1,2 +1,21 @@
 class PhysicalsController < ApplicationController
+  def new
+    @physical = Physical.new
+  end
+
+  def create
+    @physical = Physical.create(physical_params)
+    if @physical.valid?
+      @physical.save
+      redirect_to root_path
+    else
+      render new_overview_physicalfinding_path
+    end
+  end
+
+  private
+  
+  def physical_params
+    params.require(:physical).permit(:physicalfinding_measuring_date, :height, :weight, :bmi, :abdominal_circumference).merge(user_id: current_user.id, overview_id: params[:overview_id])
+  end
 end
