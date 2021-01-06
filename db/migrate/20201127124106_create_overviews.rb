4e71fe142ad7910ba9ed97e2afaa56e9d5ae6609
mayumiasakawa <m.a.mayumi214@gmail.com>
2021-01-06 16:22:54 +0900

    db re-create

--- db/migrate/20201127124106_create_overviews.rb
+++ db/migrate/20201127124106_create_overviews.rb
@@ -2,6 +2,10 @@ class CreateOverviews < ActiveRecord::Migration[6.0]
   def change
     create_table :overviews do |t|
       t.references :user      ,null: false, foreign_key: true
+      t.date :birthday
+      t.string :allergy
+      t.integer :cigarette
+      t.integer :alcohol
       t.timestamps
     end
   end
