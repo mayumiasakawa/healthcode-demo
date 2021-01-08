dce9800bf9fddbfb32d886d67e4416d4a48822ba
mayumiasakawa <m.a.mayumi214@gmail.com>
2021-01-08 21:56:55 +0900

    radio-btn.js create

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,7 @@ require("channels")
 require('./preview')
 require('./bmi')
 require('./img-clear')
+require('./radio-btn')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
