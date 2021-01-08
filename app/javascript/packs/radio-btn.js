6afb88dbd80ef1f360eabe8c484d5657c7ce2fec
mayumiasakawa <m.a.mayumi214@gmail.com>
2021-01-08 22:19:11 +0900

    allergy radio button create

---
+++ app/javascript/packs/radio-btn.js
@@ -0,0 +1,8 @@
+function changeDisabled() {
+  if ( document.Form1["number"][3].checked ) {
+      document . Form1["inputNumber"] . disabled = false;
+  } else {
+      document . Form1["inputNumber"] . disabled = true;
+  }
+}
+window.onload = changeDisabled;
