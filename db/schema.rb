2776a42985d968c987b7e7ae5a2812101219295f
mayumiasakawa <m.a.mayumi214@gmail.com>
2021-01-09 23:16:38 +0900

    form_with edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,79 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_06_062704) do
-
-  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "bloods", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "overview_id", null: false
-    t.date "blood_urine_test_date"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["overview_id"], name: "index_bloods_on_overview_id"
-    t.index ["user_id"], name: "index_bloods_on_user_id"
-  end
-
-  create_table "medicalcares", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "overview_id", null: false
-    t.date "medical_care_date"
-    t.string "clinic_name"
-    t.string "disease_name"
-    t.string "treatment_medicine"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["overview_id"], name: "index_medicalcares_on_overview_id"
-    t.index ["user_id"], name: "index_medicalcares_on_user_id"
-  end
-
-  create_table "overviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.date "birthday"
-    t.integer "allergy"
-    t.string "allergy_type"
-    t.integer "cigarette"
-    t.integer "cigarette_amount"
-    t.integer "alcohol"
-    t.integer "alcohol_amout"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_overviews_on_user_id"
-  end
-
-  create_table "physicals", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "overview_id", null: false
-    t.date "physicalfinding_measuring_date"
-    t.integer "weight"
-    t.integer "height"
-    t.integer "bmi"
-    t.integer "abdominal_circumference"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["overview_id"], name: "index_physicals_on_overview_id"
-    t.index ["user_id"], name: "index_physicals_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2020_11_19_122933) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -100,25 +28,4 @@ ActiveRecord::Schema.define(version: 2021_01_06_062704) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "vaccines", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "overview_id", null: false
-    t.date "vaccine_date"
-    t.integer "vaccine_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["overview_id"], name: "index_vaccines_on_overview_id"
-    t.index ["user_id"], name: "index_vaccines_on_user_id"
-  end
-
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "bloods", "overviews"
-  add_foreign_key "bloods", "users"
-  add_foreign_key "medicalcares", "overviews"
-  add_foreign_key "medicalcares", "users"
-  add_foreign_key "overviews", "users"
-  add_foreign_key "physicals", "overviews"
-  add_foreign_key "physicals", "users"
-  add_foreign_key "vaccines", "overviews"
-  add_foreign_key "vaccines", "users"
 end
