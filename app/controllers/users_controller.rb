b424ff7134fb58274df5fd5882de2d75c0dc7513
mayumiasakawa <m.a.mayumi214@gmail.com>
2021-01-13 18:15:52 +0900

    allergy present?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @overviews = Overview.includes(:user)
     @birthday =  Overview.includes(:user).order(birthday: :desc).where(params[:id])
+    @allergy =  Overview.includes(:user).order(allergy: :desc).where(params[:id])
 
   end
 end
