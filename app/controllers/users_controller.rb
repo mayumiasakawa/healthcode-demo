56b31d6fb9c723de935f960e6f3f907c30b77b76
mayumiasakawa <m.a.mayumi214@gmail.com>
2021-01-13 18:16:31 +0900

    alcohol present?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     @overviews = Overview.includes(:user)
     @birthday =  Overview.includes(:user).order(birthday: :desc).where(params[:id])
     @allergy =  Overview.includes(:user).order(allergy: :desc).where(params[:id])
+    @alcohol =  Overview.includes(:user).order(alcohol: :desc).where(params[:id])
 
   end
 end
