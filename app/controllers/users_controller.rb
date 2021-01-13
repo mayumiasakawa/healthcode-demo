185a828b0fc66526413467b96ea9b381314e6ce6
mayumiasakawa <m.a.mayumi214@gmail.com>
2021-01-13 18:14:59 +0900

    birthday present?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
   def show
+    @overviews = Overview.includes(:user)
+    @birthday =  Overview.includes(:user).order(birthday: :desc).where(params[:id])
+
   end
 end
