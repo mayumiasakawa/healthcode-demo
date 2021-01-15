a2f14e7b01ff1e17bae15120e6bd8b41353dbc29
mayumiasakawa <m.a.mayumi214@gmail.com>
2021-01-15 21:34:16 +0900

    radio-btn cigarette javascript create

--- app/javascript/packs/radio-btn.js
+++ app/javascript/packs/radio-btn.js
@@ -2,6 +2,9 @@ window.addEventListener('load', function(){
   const allergyYes = document.getElementById("allergy-yes");
   const allergyNo = document.getElementById("allergy-no");
   const allergyInput = document.getElementById("allergy-input");
+  const cigaretteYes = document.getElementById("cigarette-yes");
+  const cigaretteNo = document.getElementById("cigarette-no");
+  const cigaretteAmount = document.getElementById("cigarette-amount");
   const alcoholYes = document.getElementById("alcohol-yes");
   const alcoholNo = document.getElementById("alcohol-no");
   const alcoholAmount = document.getElementById("alcohol-amount");
@@ -16,6 +19,17 @@ window.addEventListener('load', function(){
   allergyYes.addEventListener('click',function(){
     if(allergyYes.checked){
       allergyInput.style.visibility ="visible";
+    }
+  });
+  
+  cigaretteNo.addEventListener('click',function(){
+    if(cigaretteNo.checked){
+      cigaretteAmount.style.visibility ="hidden";
+    }
+  });
+  cigaretteYes.addEventListener('click',function(){
+    if(cigaretteYes.checked){
+      cigaretteAmount.style.visibility ="visible";
     }
   });
   
