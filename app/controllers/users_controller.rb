0f40504ac504f4d73dfb4788b9e8a9168b3a10a5
mayumiasakawa <m.a.mayumi214@gmail.com>
2021-01-21 17:16:31 +0900

    allergy view edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @overviews = Overview.includes(:user).find(params[:id])
     @birthday =  Overview.includes(:user).order(birthday: :desc).where(params[:id])
+    @allergy =  @overviews.allergy
     @allergy_type =  @overviews.allergy_type
     @alcohol = @overviews.alcohol
     @alcohol_frequency = @overviews.alcohol_frequency
