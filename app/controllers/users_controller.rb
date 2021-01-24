fa9a03a3c5131faa7257e0bbb599e1250cc85b0a
mayumiasakawa <m.a.mayumi214@gmail.com>
2021-01-24 20:07:52 +0900

    @overview.present? create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,28 @@
 class UsersController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :move_to_index
+
   def show
-    @overviews = Overview.includes(:user).find(params[:id])
-    @birthday =  Overview.includes(:user).order(birthday: :desc).where(params[:id])
-    @allergy =  @overviews.allergy
-    @allergy_type =  @overviews.allergy_type
-    @alcohol = @overviews.alcohol
-    @alcohol_frequency = @overviews.alcohol_frequency
-    @alcohol_amount = @overviews.alcohol_amount
-    @cigarette =  @overviews.cigarette
-    @cigarette_amount =  @overviews.cigarette_amount
+      @overview = Overview.includes(:user)
+      if @overview.present?
+        @overviews = Overview.includes(:user).find(params[:id])
+        @birthday =  Overview.includes(:user).order(birthday: :desc).where(params[:id])
+        @allergy =  @overviews.allergy
+        @allergy_type =  @overviews.allergy_type
+        @alcohol = @overviews.alcohol
+        @alcohol_frequency = @overviews.alcohol_frequency
+        @alcohol_amount = @overviews.alcohol_amount
+        @cigarette =  @overviews.cigarette
+        @cigarette_amount =  @overviews.cigarette_amount
+      end
+  end
+
+  private
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to root_path
+    end
   end
 end
