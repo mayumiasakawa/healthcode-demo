e4020e070772822f3770c1e69cc89b737e76d76a
mayumiasakawa <m.a.mayumi214@gmail.com>
2021-02-06 22:15:24 +0900

    user.rb   has_one :overview edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one :overviews
+  has_one :overview
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
